2961d4b0dffbf10253918610b0a8a50627699a22
Vishnu-V-07 <vichu2838@gmail.com>
2021-04-07 20:24:02 +0530

    small changes

--- src/Strings/StockPrice.java
+++ src/Strings/StockPrice.java
@@ -12,30 +12,21 @@ public class StockPrice {
         }
         int value = 0;
         int index = 0;
-        int sell = 0;
-        int si = 0;
-        int temp = 0;
-        int profit = 0;
-        for (int i = 0; i < iNum - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                value = arr[i + 1];
-                index = i + 2;
-                // }
-                for (int j = i + 1; j < iNum - 1; j++) {
-                    if (arr[j] < arr[j + 1]) {
-                        sell = arr[j + 1];
-                        profit = sell - arr[j];
-                        // si = j + 2;
-                    }
-                    if (profit > temp) {
-                        temp = profit;
-                        si = j + 2;
-                    }
+        int max= 0;
+        
+        for(int i=0;i<iNum;i++)
+        {
+            for(int j=i+1;j<iNum;j++)
+            {
+                if(arr[j]-arr[i]>max)
+                {
+                    max=arr[j]-arr[i];
+                    value=i+1;
+                    index=j+1;
                 }
             }
-
         }
-        System.out.println("Day of buying : " + index);
-        System.out.println("Day of Selling : " + si);
+        System.out.println("Day of buying : " + value);
+        System.out.println("Day of Selling : " + index);
     }
 }
