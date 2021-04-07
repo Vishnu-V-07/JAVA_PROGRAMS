fc36bb49357c4152de2c0d393fcf0b8cee9b8813
Vishnu-V-07 <vichu2838@gmail.com>
2021-04-07 20:25:02 +0530

    07/04/2020

---
+++ src/Strings/CountOfComman.java
@@ -0,0 +1,27 @@
+package Strings;
+import java.util.*;
+public class CountOfComman{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.next();
+        int count=0;
+        ArrayList<String> al=new ArrayList();
+        for(int i=0;i<s1.length();i++){
+            for(int j=0;j<s2.length();j++)
+            {
+                if(!(al.contains(String.valueOf(s1.charAt(i)))))
+                {
+                if(s1.charAt(i)==s1.charAt(j))
+                {
+                    count++;
+                    al.add(i);
+                }
+            }
+            }
+            }
+      //  }
+        System.out.println(count);
+    }
+}
